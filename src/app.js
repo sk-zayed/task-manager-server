72c97ddc323b27c409ca809615020eb354c5a2d0
Zayed <sk.zayedd@gmail.com>
2022-12-27 00:45:05 +0530

    connected to DB

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const {connect} = require("./db/init");
 
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), "public")));
 
-app.get("/api", (req, res) => {
-    console.log("Called!");
+app.use("/api", (req, res) => {
     res.end("Helluu!!");
 });
 
 app.use(function(req, res, next) {
     res.sendFile(path.jon(process.cwd(), "public", "index.html"));
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`started on ${process.env.PORT}!!`);
 });
+
+connect()
+    .then(() => {
+            app.listen(process.env.PORT, () => {
+            console.log(`started on ${process.env.PORT}!!`);
+        });
+    })
+    .catch(error => {
+        process.exit(1)
+    });
--- src/db/init.js
+++ src/db/init.js
@@ -1,24 +1,30 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// //global configuration for queries
-// mongoose.set("returnOriginal",false);
-// mongoose.set("runValidators", true);
+//global configuration for queries
+mongoose.set("returnOriginal",false);
+mongoose.set("runValidators", true);
 
-// //creating models
+//creating models
 // require("../models/Users");
 // require("../models/Boards");
 // require("../models/Card");
 
-// const {DB_HOST, DB_NMAE} = process.env;
+const {DB_USER, DB_HOST, DB_NAME, DB_PASS} = process.env;
 
-// const connectionStr = `mongodb://${DB_HOST}/${DB_NMAE}`;
+const connectionStr = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
-// console.log("Connecting to DB...");
+console.log("Connecting to DB...");
 
-// const connect = async () => {
-//     try{
-//         await mongoose.connect()
-//     } catch {
+const connect = async () => {
+    try{
+        await mongoose.connect(connectionStr);
+        console.log(`Connected to DB ${DB_NAME}`);
+    } catch(error) {
+        console.log(error.message);
+        throw error;
+    }
+};
 
-//     }
-// };
+module.exports = {
+    connect
+};
