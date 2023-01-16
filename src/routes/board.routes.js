6aada13e03dbbf680b13b50e2c7873f95656e1a8
Zayed <sk.zayedd@gmail.com>
2023-01-16 12:08:41 +0530

    fixed update card bug

--- src/routes/board.routes.js
+++ src/routes/board.routes.js
@@ -9,7 +9,7 @@ router.post("/createboard", authenticate, BoardCtrls.createBoard);
 router.patch("/likeboard/:id", authenticate, BoardCtrls.likeBoard);
 router.get("/:id", authenticate, BoardCtrls.getBoardById);
 router.post("/createcard", authenticate, BoardCtrls.createCard);
-router.put("/updatecard", authenticate, BoardCtrls.updateCard);
+router.patch("/updatecard", authenticate, BoardCtrls.updateCard);
 router.delete("/deleteboard/:id", authenticate, BoardCtrls.deleteBoard);
 router.delete("/deletecard/:id", authenticate, BoardCtrls.deleteCard);
 router.post("/addmember", authenticate, BoardCtrls.addMember);
